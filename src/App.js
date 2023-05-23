a95f6eb3a0e1b9ef46293bd930d2a37fa68c12e1
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-05-23 12:04:05 -0600

    Segunda prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prueba
+          Prueba modificada
         </p>
         <a
           className="App-link"
